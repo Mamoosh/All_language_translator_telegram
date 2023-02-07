56b40011f7e203522c9a2a719098353ca101dcb8
U3F <98705330+Mamoosh@users.noreply.github.com>
2023-02-07 06:37:04 +0330

    Add files via upload

---
+++ translator_v2.py
@@ -0,0 +1,184 @@
+import telepot
+import time
+import urllib3
+import random, time
+from googletrans import Translator
+
+
+#change this values by yours
+
+BOT_TOKEN = ""
+ADMIN_ID_NUMBER = 000000000
+
+
+
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+}
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+
+
+bot = telepot.Bot(BOT_TOKEN)
+
+def welcome():
+    file = open('start.txt', 'r')
+    word = file.read()
+    return word
+
+def change(sentense):
+    f = open("start.txt", "w")
+    f.write(sentense)
+    f.close()
+
+LANGUAGES = {
+    'af': 'afrikaans',
+    'sq': 'albanian',
+    'am': 'amharic',
+    'ar': 'arabic',
+    'hy': 'armenian',
+    'az': 'azerbaijani',
+    'eu': 'basque',
+    'be': 'belarusian',
+    'bn': 'bengali',
+    'bs': 'bosnian',
+    'bg': 'bulgarian',
+    'ca': 'catalan',
+    'ceb': 'cebuano',
+    'ny': 'chichewa',
+    'zh-cn': 'chinese (simplified)',
+    'zh-tw': 'chinese (traditional)',
+    'co': 'corsican',
+    'hr': 'croatian',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'eo': 'esperanto',
+    'et': 'estonian',
+    'tl': 'filipino',
+    'fi': 'finnish',
+    'fr': 'french',
+    'fy': 'frisian',
+    'gl': 'galician',
+    'ka': 'georgian',
+    'de': 'german',
+    'el': 'greek',
+    'gu': 'gujarati',
+    'ht': 'haitian creole',
+    'ha': 'hausa',
+    'haw': 'hawaiian',
+    'iw': 'hebrew',
+    'he': 'hebrew',
+    'hi': 'hindi',
+    'hmn': 'hmong',
+    'hu': 'hungarian',
+    'is': 'icelandic',
+    'ig': 'igbo',
+    'id': 'indonesian',
+    'ga': 'irish',
+    'it': 'italian',
+    'ja': 'japanese',
+    'jw': 'javanese',
+    'kn': 'kannada',
+    'kk': 'kazakh',
+    'km': 'khmer',
+    'ko': 'korean',
+    'ku': 'kurdish (kurmanji)',
+    'ky': 'kyrgyz',
+    'lo': 'lao',
+    'la': 'latin',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'lb': 'luxembourgish',
+    'mk': 'macedonian',
+    'mg': 'malagasy',
+    'ms': 'malay',
+    'ml': 'malayalam',
+    'mt': 'maltese',
+    'mi': 'maori',
+    'mr': 'marathi',
+    'mn': 'mongolian',
+    'my': 'myanmar (burmese)',
+    'ne': 'nepali',
+    'no': 'norwegian',
+    'or': 'odia',
+    'ps': 'pashto',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'pa': 'punjabi',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sm': 'samoan',
+    'gd': 'scots gaelic',
+    'sr': 'serbian',
+    'st': 'sesotho',
+    'sn': 'shona',
+    'sd': 'sindhi',
+    'si': 'sinhala',
+    'sk': 'slovak',
+    'sl': 'slovenian',
+    'so': 'somali',
+    'es': 'spanish',
+    'su': 'sundanese',
+    'sw': 'swahili',
+    'sv': 'swedish',
+    'tg': 'tajik',
+    'ta': 'tamil',
+    'te': 'telugu',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'ur': 'urdu',
+    'ug': 'uyghur',
+    'uz': 'uzbek',
+    'vi': 'vietnamese',
+    'cy': 'welsh',
+    'xh': 'xhosa',
+    'yi': 'yiddish',
+    'yo': 'yoruba',
+    'zu': 'zulu',
+}
+
+
+
+def handle(msg):
+
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    translator = Translator()
+    massage = msg["text"]
+
+    if chat_id == ADMIN_ID_NUMBER and "/change" in massage :
+        substring = "/change"
+        question = massage
+        question = question.replace(substring, '')
+        try:
+            change(question)
+            bot.sendMessage(chat_id, "CHANGED!")
+        except:
+            bot.sendMessage(chat_id, "ERROR!")
+    else:
+        answer = ""
+        if "/start" in massage :
+            pm = welcome()
+            bot.sendMessage(chat_id, pm)
+        else:
+            if len(massage) < 30:
+                for language in LANGUAGES.values() :
+            
+                    question = (translator.translate(massage, dest=language)).text
+                    answer = answer + language + " : " + question + "\n"
+                bot.sendMessage(chat_id, answer)
+            else :
+                for language in LANGUAGES.values() :
+                    question = (translator.translate(massage, dest=language)).text
+                    bot.sendMessage(chat_id, (language + " : " + question))
+
+bot.message_loop(handle)
+
+print('BOT IS ONLINE !')
+
+# Keep the program running.
+while 1:
+    time.sleep(10)
